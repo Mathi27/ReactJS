b992a248c880208b6d61b52cbb4d2196c3babbaf
mathi27 <mathiyuvarajan.dev@gmail.com>
2023-04-04 11:51:51 +0530

    create basic react

---
+++ IgnitingApp/app.js
@@ -0,0 +1,15 @@
+const secondtree = React.createElement("div",{id:"parent"},[
+    React.createElement("div",{id:"child"},
+    [
+        React.createElement("h1",{},"I am a H1 Tag"),
+        React.createElement("h2",{},"I am a H2 Tag"),
+    ]),
+    React.createElement("div",{id:"child2"},
+    [
+        React.createElement("h1",{},"I am a H1 Tag"),
+        React.createElement("h2",{},"I am a H2 Tag"),
+    ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(secondtree);
